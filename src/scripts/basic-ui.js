6631902703429eaf43dc98568d066978b17b5fc1
Ali Tinai <ali-009@windowslive.com>
2023-06-15 19:39:30 +0400

    Minor adjustments to the UI

--- src/scripts/basic-ui.js
+++ src/scripts/basic-ui.js
@@ -6,12 +6,11 @@ const mainButtonList = document.querySelectorAll('.main-button')
 hamburgerButton.addEventListener('click', (e) => {
     if(!hamburgerIsOpen){
         hamburgerIsOpen=true;
-        mainButtonList.forEach((mainButton, index) => {
+        mainButtonList.forEach((mainButton) => {
             mainButton.classList.toggle('show-button')
             mainButton.style.opacity = 1;
         })
     } else {
-        //In this code block, we are simply translating our buttons in the opposite direction
         hamburgerIsOpen = false;
         mainButtonList.forEach((mainButton) => {
             mainButton.classList.toggle('show-button')
